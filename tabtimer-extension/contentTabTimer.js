acfa16a8ecfa1f1d25445fa7d30cbff5879c2262
LongNguyen <hoanglongst2000@gmail.com>
2024-05-19 03:59:16 -0700

    revert background

--- tabtimer-extension/contentTabTimer.js
+++ tabtimer-extension/contentTabTimer.js
@@ -1,24 +1,29 @@
-let alertThreshold = 0;
-let interval=10000;
+let alertThreshold = 50;
+let interval=500;
 
 // Function to retrieve alert threshold from storage and update alertThreshold
 function updateAlertThreshold() {
     chrome.storage.sync.get('alertThreshold', (data) => {
         if (data.alertThreshold) {
-            alertThreshold = data.alertThreshold * 1000; // Assuming alertThreshold is in seconds
+            alertThreshold = data.alertThreshold; // Assuming alertThreshold is in seconds
         }
         console.log("Alert Threshold updated:", alertThreshold);
-        chrome.runtime.sendMessage({ action: 'getActiveDomainTime' }, (response) =>{
-            if (response && response.elapsedTime !== undefined) {
-                const elapsedTime = response.elapsedTime;
-                if(elapsedTime > alertThreshold)
+        chrome.runtime.sendMessage({ action: 'getWindowOpenTime' }, (windowResponse) =>{
+            const windowTime = windowResponse.elapsedTime;
+            const totalBlackListTime = windowResponse.totalBlackListTime;
+            const totalWhiteListTime = windowResponse.totalWhiteListTime;
+            const totalWhiteListPercentage=(totalWhiteListTime*100)/windowTime;
+            const totalBlackListPercentage=(totalBlackListTime*100)/windowTime;
+
+                if(totalWhiteListPercentage < alertThreshold)
                 {
-                    alert("bing bong motherfker");
+                    alert("bing bong back to work motherfker");
                 }
-            }
         });
     });
 }
+
+
 
 // Run the function initially and then set it to run repeatedly every X milliseconds
 updateAlertThreshold();
